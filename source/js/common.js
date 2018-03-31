39d27f2533a80ebcdf24f7adc93a17457d06fd4a
alexander-web <alexwebq@gmail.com>
2018-03-31 20:24:01 +0300

    addede object property show func

--- source/js/common.js
+++ source/js/common.js
@@ -2,27 +2,40 @@ var text = document.querySelector('#text'),
     selectP = document.querySelector('p'),
     trigger = true;
 
-console.log(selectP)
+// console.log(selectP)
 text.innerHTML += ' hello again!!!'
 
-selectP.onclick = function(){
-    if (trigger) {
-        selectP.innerHTML = 'clicked!';
-        trigger = false;
-    } else {
-        selectP.innerHTML = 'clicked again!';
-        trigger = true;
-    }
-    
-}
+// selectP.onclick = function () {
+//     if (trigger) {
+//         selectP.innerHTML = 'clicked!';
+//         trigger = false;
+//     } else {
+//         selectP.innerHTML = 'clicked again!';
+//         trigger = true;
+//     }
+// }
 
 
 text.onclick = function () {
     if (trigger) {
-        text.style.color = 'red';
+        text.style.fontStyle = 'italic';
+        text.className = 'test2';
         trigger = false;
     } else {
-        text.style.color = 'green';
+        text.style.fontStyle = 'normal';
         trigger = true;
     }
 }
+
+function show(obj) {
+    var a = '<ul>';
+    for (i in obj) {
+        a += '<li>' + i +  ': '  + obj[i] + '</li>';
+    }
+    a += '</ul>';
+    return a;
+}
+
+
+
+selectP.innerHTML = show(text.style);
