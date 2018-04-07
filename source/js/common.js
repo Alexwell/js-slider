135dea9bc689692c765959a92f9cd34a32b602e9
AlexL <alexl88v@gmail.com>
2018-04-07 16:34:41 +0300

    Autotick added

--- source/js/common.js
+++ source/js/common.js
@@ -1,20 +1,28 @@
-var btnPrev = document.querySelector('#gallery .buttons .prev'),
-    btnNext = document.querySelector('#gallery .buttons .next'),
-    images = document.querySelectorAll('#gallery .photos img'),
-    i = 0;
+window.onload = function () {
 
-// console.log(images);
+    var btnPrev = document.querySelector('#gallery .buttons .prev'),
+        btnNext = document.querySelector('#gallery .buttons .next'),
+        images = document.querySelectorAll('#gallery .photos img'),
+        i = 0;
 
-btnPrev.onclick = function () {
-    images[i].className = '';
-    i--;
-    if (i < 0) i = images.length - 1;
-    images[i].className = 'showed';
-}
+    btnPrev.onclick = prev;
 
-btnNext.onclick = function () {
-    images[i].className = '';
-    i++;
-    if (i > images.length - 1) i = 0;
-    images[i].className = 'showed';
+    btnNext.onclick = next;
+
+    setInterval(next, 1000);
+
+    function next() {
+        images[i].className = '';
+        i++;
+        if (i > images.length - 1) i = 0;
+        images[i].className = 'showed';
+    }
+
+    function prev() {
+        images[i].className = '';
+        i--;
+        if (i < 0) i = images.length - 1;
+        images[i].className = 'showed';
+    }
+
 }
