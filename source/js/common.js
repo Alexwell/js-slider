c6b3271becbc4cfdc160ced08d52f6b044f19280
AlexL <alexl88v@gmail.com>
2018-04-07 17:15:06 +0300

    test callback added

--- source/js/common.js
+++ source/js/common.js
@@ -3,19 +3,17 @@ window.onload = function () {
     var btnPrev = document.querySelector('#gallery .buttons .prev'),
         btnNext = document.querySelector('#gallery .buttons .next'),
         images = document.querySelectorAll('#gallery .photos img'),
-        i = 0;
+        i = 0,
+        callbackCounter = 0;
 
     btnPrev.onclick = prev;
 
     btnNext.onclick = next;
 
-    setInterval(next, 1000);
+    var testInterval = setInterval(next, 2000);
 
     function next() {
-        images[i].className = '';
-        i++;
-        if (i > images.length - 1) i = 0;
-        images[i].className = 'showed';
+        listNext(testCallback);
     }
 
     function prev() {
@@ -23,6 +21,21 @@ window.onload = function () {
         i--;
         if (i < 0) i = images.length - 1;
         images[i].className = 'showed';
+    }
+
+    function listNext() {
+        images[i].className = '';
+        i++;
+        if (i > images.length - 1) {
+            i = 0;
+            testCallback();
+        }
+        images[i].className = 'showed';
+    }
+
+    function testCallback() {
+        callbackCounter++;
+        console.log("Hello from callback " + callbackCounter + "!")
     }
 
 }
