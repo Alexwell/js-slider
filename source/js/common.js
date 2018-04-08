a45d601e43628f41e4b5d5f0bf2cc931386f13ca
AlexL <alexl88v@gmail.com>
2018-04-08 17:12:50 +0300

    OOP style start

--- source/js/common.js
+++ source/js/common.js
@@ -1,42 +1,83 @@
 window.onload = function () {
 
-    var btnPrev = document.querySelector('#gallery .buttons .prev'),
-        btnNext = document.querySelector('#gallery .buttons .next'),
-        images = document.querySelectorAll('#gallery .photos img'),
-        i = 0,
-        callbackCounter = 0;
+    // var btnPrev = document.querySelector('#gallery .buttons .prev'),
+    //     btnNext = document.querySelector('#gallery .buttons .next'),
+    //     images = document.querySelectorAll('#gallery .photos img'),
+    //     i = 0,
+    //     callbackCounter = 0;
 
-    btnPrev.onclick = prev;
+    // btnPrev.onclick = prev;
 
-    btnNext.onclick = next;
+    // btnNext.onclick = next;
 
-    var testInterval = setInterval(next, 1000);
+    // var testInterval = setInterval(next, 1000);
 
-    function next() {
-        listNext(testCallback);
-    }
+    // function next() {
+    //     listNext(testCallback);
+    // }
 
-    function prev() {
-        images[i].className = '';
-        i--;
-        if (i < 0) i = images.length - 1;
-        images[i].className = 'showed';
-    }
+    // function prev() {
+    //     images[i].className = '';
+    //     i--;
+    //     if (i < 0) i = images.length - 1;
+    //     images[i].className = 'showed';
+    // }
 
-    function listNext(f) {
-        var callback = f || function () {};
-        images[i].className = '';
-        i++;
-        if (i > images.length - 1) {
-            i = 0;
-            callback();
+    // function listNext(f) {
+    //     var callback = f || function () {};
+    //     images[i].className = '';
+    //     i++;
+    //     if (i > images.length - 1) {
+    //         i = 0;
+    //         callback();
+    //     }
+    //     images[i].className = 'showed';
+    // }
+
+    // function testCallback() {
+    //     callbackCounter++;
+    //     console.log("Hello from callback " + callbackCounter + " !")
+    // }
+ 
+    var images = document.querySelectorAll('.gallery-1 .photos img');
+
+    var slider = new Slider(images);
+
+    document.querySelector('.gallery-1 .buttons .prev').onclick = function () {
+        slider.prev();
+    };
+    document.querySelector('.gallery-1 .buttons .next').onclick = function () {
+        slider.next();
+    };
+
+    var images2 = document.querySelectorAll('.gallery-2 .photos img');
+    var slider2 = new Slider(images2);
+
+    document.querySelector('.gallery-2 .buttons .prev').onclick = function () {
+        slider2.prev();
+    };
+    document.querySelector('.gallery-2 .buttons .next').onclick = function () {
+        slider2.next();
+    };
+
+    function Slider(images) {
+        this.images = images;
+        this.i = 0
+        this.prev = function () {
+            this.images[this.i].classList.remove('showed');
+            this.i--;
+            if (this.i < 0) {
+                this.i = this.images.length - 1;
+            }
+            this.images[this.i].classList.add('showed');
         }
-        images[i].className = 'showed';
+        this.next = function () {
+            this.images[this.i].classList.remove('showed');
+            this.i++;
+            if (this.i >= this.images.length) {
+                this.i = 0;
+            }
+            this.images[this.i].classList.add('showed');
+        }
     }
-
-    function testCallback() {
-        callbackCounter++;
-        console.log("Hello from callback " + callbackCounter + " !")
-    }
-
 }
